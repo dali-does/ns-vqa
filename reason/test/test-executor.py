e6352988d7e3c9819dab45ba09b22c6f3f353e10
dali <dali@cs.umu.se>
2023-03-27 11:08:08 +0000

    Added passing tests for attributes as part of the vocab with remove.

--- reason/test/test-executor.py
+++ reason/test/test-executor.py
@@ -16,8 +16,6 @@ class TestOriginalClevr(unittest.TestCase):
         index = 0
         split = 'val'
         ans = self.executor.run(x,index,split)
-        # if ans == 'error':
-        #     print(self.executor.exe_trace)
         self.assertEquals(ans, 'no')
 
 class TestMultihop(unittest.TestCase):
@@ -55,7 +53,30 @@ class TestMultihop(unittest.TestCase):
         ans = self.executor.run(x,index,split)
         self.assertEquals(ans, '5')
 
+class TestAttributes(unittest.TestCase):
+    data_path = 'data'
 
+    def setUp(self):
+        scene_json = '{}/val-scenes.json'.format(self.data_path)
+        vocab_json = '{}/vocab-remove.json'.format(self.data_path)
+        self.executor = ClevrExecutor(scene_json, scene_json, vocab_json)
+
+
+    def test_remove_two(self):
+        # count, remove, red, cube, remove, purple, sphere, scene, end
+        x = [6, 14, 13, 7, 14, 12, 16, 15, 2]
+        index = 0
+        split = 'val'
+        ans = self.executor.run(x,index,split, use_attributes=True)
+        self.assertEquals(ans, '4')
+
+    def test_remove_many(self):
+        # count, remove, cylinder, remove, purple, sphere, scene, end
+        x = [6, 14, 9, 14, 12, 16, 15, 2]
+        index = 3
+        split = 'val'
+        ans = self.executor.run(x,index,split, use_attributes=True)
+        self.assertEquals(ans, '2')
 
 
 if __name__ == '__main__':
